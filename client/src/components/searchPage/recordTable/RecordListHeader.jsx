ac5ec3852d704691614ae2d15ea8dfba668c5b9f
Dawei Fu <fudw@pku.edu.cn>
2025-12-05 16:55:36 +0800

    update table header

--- client/src/components/searchPage/recordTable/RecordListHeader.jsx
+++ client/src/components/searchPage/recordTable/RecordListHeader.jsx
@@ -22,7 +22,7 @@ const RecordListHeader = (props) => {
     const header = props.columns.filter(col => col.isVisible == true)
         .map((col, i) =>
             (<th key={i} style={styles.th} onClick={props.onColumnHeaderClick(col.name)} role="button">
-                <div style={styles.left}>{col.name}</div>
+                <div style={styles.left}>{col.title || col.name}</div>
                 <div style={styles.right}>
                     <span className="glyphicon glyphicon-triangle-top" aria-hidden="true"
                         style={col.name === ordFieldName && ordDirection === -1 ? {} : styles.orderInactive} />
